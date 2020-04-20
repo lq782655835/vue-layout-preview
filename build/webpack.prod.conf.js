2a4e89c33ea53684e1c124ea84faec69f8c7913b
springleo <782655835@qq.com>
2020-04-20 20:32:48 +0800

    refactor: webpack publicPath && del no use image

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -24,7 +24,8 @@ var webpackConfig = merge(baseWebpackConfig, {
   output: {
     path: config.build.assetsRoot,
     filename: utils.assetsPath('js/[name].[chunkhash].js'),
-    chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
+    chunkFilename: utils.assetsPath('js/[id].[chunkhash].js'),
+    publicPath: '/vue-layout-preview/'
   },
   plugins: [
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
